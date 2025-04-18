ee2238c8744bd71eed9fc54525852bc5b11d57ef
Henrijs Kravals <henrijskravals@gmail.com>
2025-04-18 17:30:41 +0300

    clean project

---
+++ app/Http/Controllers/EventController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Event;
+
+class EventController extends Controller
+{
+    public function index()
+    {
+        $events = Event::all();
+        return view('events.index', ['events' => $events]);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,18 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ItemController;
+use App\Http\Controllers\EventController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('home');
-});
+    return view('home'); 
+})->name('home'); // to link it with blade
 
 Route::get('/dashboard', function () {
     return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+})->name('dashboard');
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
+Route::get('/items', [ItemController::class, 'index'])->name('items.index');
+Route::get('/events', [EventController::class, 'index'])->name('events.index');
 
-require __DIR__.'/auth.php';
